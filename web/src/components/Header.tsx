88a9effeaa11a087c226a677176bc2f3cb2aa0e4
JoaoOliveiraZz <linajoaovictor@gmail.com>
2023-01-20 17:03:23 -0300

    Modal de criação de hábito parcialmente criado

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,15 +1,41 @@
 import logoImage from '../assets/Logo.svg'
-import { Plus } from 'phosphor-react'
+import { Plus, X } from 'phosphor-react'
+import { useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewHabitForm } from './newHabitForm';
 
 export function Header() {
 
-    return(
+
+
+    return (
         <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
             <img src={logoImage} alt="Habits" />
-            <button
-                type='button'
-                className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 transition-al duration-300 hover:border-violet-300'
-            > <Plus size={20} className='text-violet-500' /> Novo hábito</button>
+
+            <Dialog.Root>
+
+                <Dialog.Trigger
+                    // onClick={buttonClicked}
+                    type='button'
+                    className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 transition-al duration-300 hover:border-violet-300'
+                ><Plus size={20} className='text-violet-500' /> Novo hábito</Dialog.Trigger>
+
+                <Dialog.Portal>
+                    <Dialog.Overlay className='w-screen h-screen fixed bg-black/80 inset-0 ' />
+                    <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                        <Dialog.Close className='absolute top-6 right-6 text-zinc-400 hover:text-zinc-200'>
+                            <X size={20} aria-label='Fechar' />
+                        </Dialog.Close>
+                        <Dialog.Title className='text-3xl leading-tight font-extrabold'>
+                            Criar Hábito
+                        </Dialog.Title>
+                        
+                        <NewHabitForm />
+
+                    </Dialog.Content>
+                </Dialog.Portal>
+            </Dialog.Root>
+
         </div>
     )
 
