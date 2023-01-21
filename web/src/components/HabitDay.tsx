3c4320077be51fac6530dae101f7e3ffed6ab645
JoaoOliveiraZz <linajoaovictor@gmail.com>
2023-01-21 18:55:08 -0300

    Checkbox criadas e coletando dados do formulário

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -6,39 +6,62 @@ interface HabitProps {
 import * as Popover from "@radix-ui/react-popover";
 import { ProgressBar } from "./progressBar";
 import clsx from 'clsx'
+import * as Checkbox from '@radix-ui/react-checkbox'
+import { Check } from "phosphor-react";
 
-export function Habit (props: HabitProps){
+export function Habit(props: HabitProps) {
 
     const completedPorcentage = Math.round((props.completed / props.amount) * 100)
 
     return (
         <Popover.Root>
-            <Popover.Trigger 
-                className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg', 
-                {
-                    'bg-violet-500 border-violet-400': completedPorcentage >= 80,
-                    'bg-violet-600 border-violet-500': completedPorcentage >= 60 && completedPorcentage <80,
-                    'bg-violet-700 border-violet-500': completedPorcentage >= 40 && completedPorcentage < 60,
-                    'bg-violet-800 border-violet-600': completedPorcentage >= 20 && completedPorcentage < 40,
-                    'bg-violet-900 border-violet-700': completedPorcentage >= 0 && completedPorcentage < 20,
-                    'bg-zinc-900 border-violet-800': completedPorcentage == 0
-                })
-            }
+            <Popover.Trigger
+                className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg',
+                    {
+                        'bg-violet-500 border-violet-400': completedPorcentage >= 80,
+                        'bg-violet-600 border-violet-500': completedPorcentage >= 60 && completedPorcentage < 80,
+                        'bg-violet-700 border-violet-500': completedPorcentage >= 40 && completedPorcentage < 60,
+                        'bg-violet-800 border-violet-600': completedPorcentage >= 20 && completedPorcentage < 40,
+                        'bg-violet-900 border-violet-700': completedPorcentage >= 0 && completedPorcentage < 20,
+                        'bg-zinc-900 border-violet-800': completedPorcentage == 0
+                    })
+                }
 
             />
-            
+
 
             <Popover.Portal>
                 <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-                        <span className="font-semibold text-zinc-400 ">Segunda-feira</span>
-                        <span className="mt-1 font-extrabold leadin-tight text-xl">20/01</span>
+                    <span className="font-semibold text-zinc-400 ">Segunda-feira</span>
+                    <span className="mt-1 font-extrabold leadin-tight text-xl">20/01</span>
 
-                        <ProgressBar progress={completedPorcentage} />
+                    <ProgressBar progress={completedPorcentage} />
 
-                        <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+                    <div className="mt-6 flex flex-col gap-3">
+
+                        <Checkbox.Root className="flex items-center gap-3 group">
+                            <div 
+                                className="h-8 rounded-lg w-8 flex items-center justify-center bg-zinc-900 border-2 border-zinc-800
+                                group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500
+                            ">
+                                <Checkbox.Indicator >
+                                    <Check 
+                                        size={20}
+                                        className= 'text-white'
+                                    />
+                                </Checkbox.Indicator>
+                            </div>
+                            <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                                Beber 2l de água
+                            </span>
+                        </Checkbox.Root>
+
+                    </div>
+
+                    <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
                 </Popover.Content>
             </Popover.Portal>
-        </Popover.Root>
+        </Popover.Root >
     )
 
 }
--- web/src/components/newHabitForm.tsx
+++ web/src/components/newHabitForm.tsx
@@ -1,24 +1,90 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+import { FormEvent, useState } from "react";
+
+const avaliableWeekDays = [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado'
+]
 
 
-export function NewHabitForm(){
+export function NewHabitForm() {
+
+    const [title, setTitle] = useState();
+    const [weekDays, setWeekDays] = useState<number[]>([]);
+
+    function handleToggleWeekDay(weekDay: number) {
+
+        if (weekDays.includes(weekDay)) {
+            const removedOndeWeekDays = weekDays.filter(day => day != weekDay);
+
+            setWeekDays(removedOndeWeekDays)
+        } else {
+            const weekDaysAdd = [...weekDays, weekDay]
+
+            setWeekDays(weekDaysAdd)
+        }
+
+    }
+
+    function createNewHabit(event: FormEvent){
+        event.preventDefault()
+
+        console.log(title)
+        console.log(weekDays)
+    }
 
     return (
-        <form className="w-full flex flex-col mt-6 ">
+        <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6 ">
             <label htmlFor="title" className="font-semibold leading-tight">
                 Qual seu comprometimento?
             </label>
-            <input 
+            <input
                 type="text"
                 id="title"
                 placeholder="Exercícios, Dormir bem, saúde, etc..."
                 autoFocus
                 className="p-4 rounded-lg mt-3 bg-zinc-800 text-withe placeholder:text-zinc-400"
+                onChange={event => setTitle(event.target.value)}
             />
 
             <label htmlFor="" className="font-semibold leading-tight mt-4">
                 Qual a recorrência?
             </label>
+
+            <div className="mt-3 flex flex-col gap-2" >
+                {
+                    avaliableWeekDays.map((avaliableWeekDay, index) => {
+                        return (
+                            <Checkbox.Root
+                                key={avaliableWeekDay}
+                                className="flex items-center gap-3 group"
+                                onCheckedChange={ () => handleToggleWeekDay(index) }
+                            >
+                                <div
+                                    className="h-8 rounded-lg w-8 flex items-center justify-center bg-zinc-900 border-2 border-zinc-800
+                                        group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500
+                                    ">
+                                    <Checkbox.Indicator >
+                                        <Check
+                                            size={20}
+                                            className='text-white'
+                                        />
+                                    </Checkbox.Indicator>
+                                </div>
+                                <span className=" text-white leading-tight">
+                                    {avaliableWeekDay}
+                                </span>
+                            </Checkbox.Root>
+                        )
+                    })
+                }
+            </div>
 
             <button type="submit" className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-500">
                 <Check size={20} weight='bold' />
